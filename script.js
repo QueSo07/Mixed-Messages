0daa065664c00147d23298c9d42235c226a33183
QueSo07 <144483603+QueSo07@users.noreply.github.com>
2024-02-19 02:31:13 -0500

    Update script.js
    Update exercises to workouts with multiple exercise and random reps assigned to each.
    
    Workouts are listed as Exercise A, Exercise B, Exercise C , etc
    
    Add greeting message and subtle warning with rest time

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-const exercises = ['Jumping Jacks', 'Squats', 'Butt Kicks', 'Push Ups', 'Crunches'];
-const minReps = 10
-const maxReps = 20
+// Workouts with minimum 5 diiferent exercises
+const workoutGroup =[ 
+                  ['Jumping Jacks', 'Squats', 'Butt Kicks', 'Push-ups', 'Crunches', 'Mountain Climbers'],
+                  ['Seal Claps', 'Lunges', 'Diamond Push-ups', 'Toe Touches', 'Triangle Crunches'],
+                  ['X Jumps', 'Side Lunges', 'Judo Push-ups', 'Bicycle Crunches', 'Russian Twists'],
+];
+// Variables for min and max reps
+const minReps = 15;
+const maxReps = 25;
 
+// function to genrate random number of reps
 const randomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-const randomExercise = arr => {
-    const workout = Math.floor(Math.random() * arr.length);
-    return arr[workout];
+ 
+// function to generate a random exercise
+const randomExercise = workoutGroup => {
+    const exercise = Math.floor(Math.random() * workoutGroup.length);
+    return workoutGroup[exercise];
 }
 
-const randomWorkout = (function1, function2) => {
-    const exercise = randomExercise(exercises);
-    const numReps = randomNumber(minReps,maxReps);
-    return `Hello. Your exercise for today is ${numReps} ${exercise}. You can start when you're ready!!`;
+// function to generate a random workout with exercises and reps
+const generateRandomWorkout = () => {
+    const exerciseGroup = randomExercise(workoutGroup);
+    let workout = "Hello! Here is your workout for today. Remember to go at an easy pace if you are just starting out.\nRest 15 secs in between.\n \n";
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    for(let i=0; i < exerciseGroup.length; i++) {
+        const numReps = randomNumber(minReps,maxReps);
+        workout += `Exercise ${alphabet[i]}: ${numReps} ${exerciseGroup[i]}.\n`;
+    }
+    return workout 
 }
 
-console.log(randomWorkout());
+
+console.log(generateRandomWorkout());
